3ddc3fe2b3221cd336a163ad9415b3a5e8a03f11
altocas <altocas.jalo@gmail.com>
2025-10-03 20:03:41 +0000

    correção feita com sucesso

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,6 @@
 INSERT INTO tb_categoria(descricao) VALUES ('Curso');
 INSERT INTO tb_categoria(descricao) VALUES ('Oficina');
 
-INSERT INTO tb_bloco(inicio,fim) VALUES ('2017-09-25 08:00:00','2017-09-25 11:00:00');
-INSERT INTO tb_bloco(inicio,fim) VALUES ('2017-09-25 14:00:00','2017-09-25 18:00:00');
-INSERT INTO tb_bloco(inicio,fim) VALUES ('2017-09-26 08:00:00','2017-09-26 11:00:00');
-
 INSERT INTO tb_participante(nome,email) VALUES ('José Silva','jose@gmail.com');
 INSERT INTO tb_participante(nome,email) VALUES ('Tiago Faria','tiago@gmail.com');
 INSERT INTO tb_participante(nome,email) VALUES ('Maria do Rosario','maria@gmail.com');
@@ -13,6 +9,9 @@ INSERT INTO tb_participante(nome,email) VALUES ('Teresa Silva','teresa@gmail.com
 INSERT INTO tb_atividade(nome,descricao,preco,categoria_id) VALUES ('Curso de HTML','Aprenda HTMl de forma pratica',80.00,1);
 INSERT INTO tb_atividade(nome,descricao,preco,categoria_id) VALUES ('Oficina de Github','controle versões de seus projetos',50.00,2);
 
+INSERT INTO tb_bloco(inicio,fim,atividade_id) VALUES ('2017-09-25 08:00:00','2017-09-25 11:00:00',1);
+INSERT INTO tb_bloco(inicio,fim,atividade_id) VALUES ('2017-09-25 14:00:00','2017-09-25 18:00:00',2);
+INSERT INTO tb_bloco(inicio,fim,atividade_id) VALUES ('2017-09-26 08:00:00','2017-09-26 11:00:00',2);
 
 INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES (1, 1);
 INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES (1, 2);
